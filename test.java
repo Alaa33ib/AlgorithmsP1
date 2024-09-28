e7637ddfed3822be0579f2982e30ecf21b9720f9
Alaa33ib <alaa33ib@gmail.com>
2024-09-28 17:26:51 +0300

    Update test.java

--- test.java
+++ test.java
@@ -1,5 +1,38 @@
 import java.util.*
+import java.lang.Math;
 
+
+
+
+public static void main(){
+
+	String Name[]; 		//list of all employyes
+	int id[];           //list of all ids
+	int seperiorId[];   //list of seperior ids
+	int skills[];       //list of skill levels
+
+
+
+
+
+public class team{
+	int memberids[];
+	int totalSkills;
+	int broadSkillSet;
+
+	public team(int m1, int m2){
+		memberids = {m1,m2};
+		totalSkills = m1.skills + m2.skills;
+		broadSkillSet = Math.abs(m1.skills - m2.skills);
+
+	}
+
+}
+
+}
 public class BruteForce {
+
+team teams[];
+
 
 }
