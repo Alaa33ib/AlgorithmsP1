68826c05750e593f6c17c686c69d49a954f3686f
Alaa33ib <alaa33ib@gmail.com>
2024-10-11 15:39:21 +0300

    Update BruteFrorceTeamFormation.java

--- BruteFrorceTeamFormation.java
+++ BruteFrorceTeamFormation.java
@@ -5,13 +5,13 @@ import java.util.*;
 
 public class BruteFrorceTeamFormation{
 
-	public class Employee{
-		String name;
-		int skillLevel;
-		int id;
-		int supervisor;
+	public static class Employee{
+		private String name;
+		private int skillLevel;
+		private int id;
+		private Integer supervisor;
 
-		public Employee(String name, int id, int skillLevel, int supervisor){
+		public Employee(String name, int id, int skillLevel, Integer supervisor){
 			this.name = name;
 			this.id = id;
 			this.skillLevel = skillLevel;
@@ -19,12 +19,41 @@ public class BruteFrorceTeamFormation{
 		}
 
 	}
+   
+   
 
 	public static void main(String[] args){
 		List<Employee> Employees = new ArrayList<>();
+      Scanner scanner = new Scanner(System.in);
+      System.out.print("Please enter the size: ");
+      int maxSize = scanner.nextInt();
+      scanner.nextLine();
+      System.out.print("Please enter file name: ");
+      String file = scanner.nextLine();
+
 		//read employees from file
-		int maxSize = 2;
-		Team BestTeam = BruteForceResults(Employees, maxSize) 
+      try(BufferedReader Br = new BufferedReader(new FileReader(file))){
+        String Line ;
+        while((Line = Br.readLine() ) != null){
+            String [] cutpart = Line.split(":"); // Array of String to store and part the info from the file 
+
+            Integer SupervisorId = Integer.parseInt(cutpart[0].trim());
+            String Name = cutpart[1].trim();
+            int ID = Integer.parseInt(cutpart[2].trim());
+            int skillLevel = Integer.parseInt(cutpart[3].trim());
+
+            Employee employee = new Employee(Name, ID, skillLevel, SupervisorId);
+            Employees.add(employee);
+        }
+      }
+      catch (IOException e) {
+       e.printStackTrace();
+      }
+
+      // end of reading
+            
+      BruteFrorceTeamFormation formation = new BruteFrorceTeamFormation();
+		Team BestTeam = formation.BruteForceResults(Employees, maxSize);
 
 		if (BestTeam != null) {
             System.out.println("The optimal team is: " + BestTeam.getMembers());
@@ -56,25 +85,27 @@ public class BruteFrorceTeamFormation{
 	}
 
 	public Team BruteForceResults(List<Employee> Employees, int maxSize){
-		Team BestTeam = null;
-		int maxSkills = 0;
+		 BestTeam = null;
+		 maxSkills = 0;
 
 		List<Employee> currentCombo = new ArrayList<>();
 		for(int size = 2; size<=maxSize; size++){
-			FindOptimalTeamCombo(Employees, currentCombo, size, 0)
+			FindOptimalTeamCombo(Employees, currentCombo, size, 0);
 		}
+		return BestTeam;
 
 	}
 
-	static Team BestTeam = null;
-	static int maxSkills = 0;
+	 Team BestTeam = null;
+	 int maxSkills = 0;
 
 	public void FindOptimalTeamCombo(List<Employee> Employees, List<Employee> currentCombo, int size, int start){
 		if (currentCombo.size()==size) {
+      
 			if (isTeamValid(currentCombo)){
 				int totalSkill = 0;
-				int maxSkill = 100; 
-				int minSkill = 0;
+				int maxSkill = 0; 
+				int minSkill = 100;
 
 				for(int i = 0; i<currentCombo.size(); i++){
 					int skillLevel = currentCombo.get(i).skillLevel;
@@ -83,7 +114,7 @@ public class BruteFrorceTeamFormation{
 					if(skillLevel<minSkill) minSkill = skillLevel;
 				}
 				int skillDifference = maxSkill - minSkill;
-
+            
 				if (totalSkill > maxSkills) {
 					maxSkills = totalSkill;
 					BestTeam = new Team(getEmpNames(currentCombo), totalSkill, skillDifference);
@@ -92,7 +123,7 @@ public class BruteFrorceTeamFormation{
 			}
 			return;
 		}
-		for(int i = start; ii<Employees.size(); i++){
+		for(int i = start; i<Employees.size(); i++){
 			currentCombo.add(Employees.get(i));
 			FindOptimalTeamCombo(Employees, currentCombo, size, i+1);
 			currentCombo.remove(currentCombo.size()-1);
@@ -102,7 +133,7 @@ public class BruteFrorceTeamFormation{
 
 	public boolean isTeamValid(List<Employee> team){
 		for(int i = 0; i<team.size(); i++){
-			Employee emp1 = team.get(i)
+			Employee emp1 = team.get(i);
 
 			for(int j = 0; j<team.size(); j++){
 				Employee emp2 = team.get(j);
